6961d8f7c41187ca7f1673a4d37b9ddd4c4374fa
dongun0116 <dongun16@naver.com>
2024-10-29 22:43:44 +0900

    2024.10.29-2nd-challange-recursion-ex

---
+++ challengeEx/recursion/factorial.js
@@ -0,0 +1,9 @@
+//숫자를 받아 해당 숫자의 계승을 반환하는 팩토리얼 함수
+function factorial(num) {
+    // 종료조건 num이 1보다 작을경우 1을 반환
+    if (num < 2) return 1;
+    // 재귀방식 num을 곱하고 감소시켜서 반환
+    return num * factorial(num - 1);
+}
+
+console.log(factorial(4));
---
+++ challengeEx/recursion/power.js
@@ -0,0 +1,9 @@
+//밑과 지수를 받아들이고 밑의 거듭제곱을 지수로 반환하는 함수
+function power(num1, num2) {
+    // 종료조건 지수가 0이면 1반환
+    if (num2 === 0) return 1;
+    // 밑을 곱하고 지수를 감소시키고 반환
+    return num1 * power(num1, num2 - 1);
+}
+
+console.log(power(2, 4));
