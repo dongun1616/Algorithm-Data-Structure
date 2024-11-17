b23428e5b56f084350a1ee22e312e8e736ec8260
dongun0116 <dongun16@naver.com>
2024-11-17 22:33:29 +0900

    2024.11.17-2nd-dataStructure-doublyLinkedList-get,set,insert,remove

--- dataStructure/doublyLinkedList/doublyLinkedList.js
+++ dataStructure/doublyLinkedList/doublyLinkedList.js
@@ -103,10 +103,100 @@ class DoublyLinkedList {
         this.length++;
         return this;
     }
+    //get 인덱스를 인자로 받아 노드를 반환하는 메소드
+    get(index) {
+        //index가 음수이거나 리스트의 길이보다 크거나 같으면 null반환
+        if (index < 0 || index >= this.length) return null;
+        //인덱스가 리스트의 길이의 중간지점보다 작으면 헤드부터
+        if (index <= this.length / 2) {
+            let headCounter = 0;
+            //리스트 순회
+            let current = this.head;
+            //카운터와 인덱스가 같지 않는 경우 순회
+            while (headCounter !== index) {
+                current = current.next;
+                //카운트 증가
+                headCounter++;
+            }
+            return current;
+        }
+        //인덱스가 리스트의 길이의 중간지점보다 크면 태일부터
+        else {
+            let tailCounter = this.length - 1;
+            //리스트 순회
+            let current = this.tail;
+            //카운터와 인덱스가 같지 않는 경우 순회
+            while (tailCounter !== index) {
+                current = current.prev;
+                //카운트 증가
+                tailCounter--;
+            }
+            return current;
+        }
+    }
+    //set 인덱스와 값을 인자로 받아 해당 인덱스에 값을 수정하는 메소드
+    set(index, val) {
+        //찾는 노드의 위치를 get메소드로 선언
+        let foundNode = this.get(index)
+        if (foundNode) {
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
+    //insert 인덱스와 값을 인자로 받아 해당 인덱스에 값을 추가하고 앞뒤를 연결하는 메소드
+    insert(index, val) {
+        //index가 음수이거나 리스트의 길이보다 크면 false를 반환
+        if (index < 0 || index > this.length) return false
+        //index가 리스트의 길이와 같으면 push를 호출
+        if (index === this.length) return !!this.push(val) //!!는 값이 존재하면 true를 반환
+        //index가 0이면 unshift를 호출
+        if (index === 0) return !!this.unshift(val)
+        //노드를 생성
+        let newNode = new Node(val)
+        //리스트 중간에 추가하려면 get을 index-1로 호출
+        let prevNode = this.get(index - 1)
+        let nextNode = prevNode.next
+        //앞에 노드와 연결
+        prevNode.next = newNode;
+        newNode.prev = prevNode;
+        //뒤에 노드와 연결
+        newNode.next = nextNode;
+        nextNode.prev = newNode;
+        //길이 증가
+        this.length++;
+        return true
+    }
+    //remove 인덱스를 인자로 받아 해당 인덱스에 노드를 삭제하고 앞뒤를 연결하는 메소드
+    remove(index) {
+        //index가 0보다 작거나 길이보다 크거나 같으면 undefined 반환
+        if (index < 0 || index >= this.length) return undefined;
+        //index가 길이-1과 같으면 pop 호출
+        if (index === this.length - 1) return this.pop()
+        //index가 0이면 shift 호출
+        if (index === 0) return this.shift();
+        //제거할 노드 변수 선언
+        let removedNode = this.get(index)
+        //제거할 노드의 앞 뒤 노드 변수 선언
+        let prevNode = removedNode.prev;
+        let nextNode = removedNode.next
+        //제거할 노드 앞뒤 노드들을 연결
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+        //삭제할 노드 앞뒤를 null로 선언
+        removedNode.next = null;
+        removedNode.prev = null;
+        //길이 감소
+        this.length--;
+        //삭제한 노드 반환
+        return removedNode;
+    }
 }
 
 let list = new DoublyLinkedList();
-list.push("second")
-list.push("third")
-console.log(list.unshift('first'));
-console.log(list);
+list.push("one")
+list.push("two")
+list.push("lolol")
+list.push("three")
+console.log(list.remove(2));
+console.log(list.get(2));
