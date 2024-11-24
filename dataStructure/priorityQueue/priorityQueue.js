3dec03b21e559e3d8830f4441d5eff82494e30c7
dongun0116 <dongun16@naver.com>
2024-11-24 22:28:56 +0900

    2024.11.24-1st-binaryHeap,priorityQueue-ex

--- dataStructure/priorityQueue/priorityQueue.js
+++ dataStructure/priorityQueue/priorityQueue.js
@@ -1,14 +1,24 @@
-//최대 이진 힙
+//우선순위 큐
+//최소 이진 힙을 활용
 
-//이진 힙 클래스
-class MaxBinaryHeap {
-    constructor() {
-        this.vals = [55, 39, 41, 18, 27, 12, 33];
+//노드 클래스
+class Node {
+    constructor(val, priority) {
+        this.val = val;
+        this.priority = priority;
     }
-    //노드를 추가하고 버블 업을 이용해 알맞은 위치로 이동시키는 메소드
-    insert(element) {
+}
+//우선순위 큐 클래스
+class PriorityQueue {
+    constructor() {
+        this.vals = [];
+    }
+    //노드를 추가하고 버블 업을 이용해 우선순위에 맞는 위치로 이동시키는 메소드
+    enqueue(val, priority) {
+        //새로운 노드 추가
+        let newNode = new Node(val, priority)
         //해당 요소 배열에 추가
-        this.vals.push(element);
+        this.vals.push(newNode);
         //위치를 찾는 버블업 메소드 호출
         this.bubbleUp();
         return this.vals
@@ -24,8 +34,8 @@ class MaxBinaryHeap {
             //부모 인덱스와 값 선언
             let parentIdx = Math.floor((idx - 1) / 2)
             let parent = this.vals[parentIdx]
-            //추가하는 값이 부모 값보다 작거나 같으면 break
-            if (element <= parent) break;
+            //element의 우선순위가 부모의 우선순위 보다 크거나 같으면 break
+            if (element.priority >= parent.priority) break;
             //추가하는 값이 부모 값보다 크면 스왑
             this.vals[parentIdx] = element;
             this.vals[idx] = parent;
@@ -33,12 +43,12 @@ class MaxBinaryHeap {
             idx = parentIdx
         }
     }
-    //힙에서 최대 값 노드(루트)를 제거하고, 버블 다운으로 제조정하는 메소드
-    extractMax() {
+    //큐에서 (우선순위)최솟값을 제거하고, 버블 다운으로 제조정하는 메소드
+    dequeue() {
         //값이 없으면 null반환
         if (this.vals.length === 0) return null;
-        //최댓값(루트) 저장
-        const max = this.vals[0]
+        //최솟값 저장
+        const min = this.vals[0]
         //끝 값 저장
         const end = this.vals.pop();
         //끝 값을 뺀 이후에도 값이 있으면
@@ -49,7 +59,7 @@ class MaxBinaryHeap {
             this.bubbleDown();
         }
         //삭제한 값 반환
-        return max;
+        return min;
     }
     //버블 다운
     bubbleDown() {
@@ -70,14 +80,14 @@ class MaxBinaryHeap {
             //왼쪽 자식이 존재하면 자식 값 선언
             if (leftChildIdx < length) {
                 leftChild = this.vals[leftChildIdx];
-                if (leftChild > element) swap = leftChildIdx;
+                if (leftChild.priority < element.priority) swap = leftChildIdx;
             }
             //오른쪽 자식이 존재하면 자식 값 선언
             if (rightChildIdx < length) {
                 rightChild = this.vals[rightChildIdx];
                 if (
-                    (swap === null && rightChild > element) ||
-                    (swap !== null && rightChild > leftChild)
+                    (swap === null && rightChild.priority < element.priority) ||
+                    (swap !== null && rightChild.priority < leftChild.priority)
                 ) {
                     swap = rightChildIdx;
                 }
@@ -92,6 +102,15 @@ class MaxBinaryHeap {
         }
     }
 }
-let heap = new MaxBinaryHeap();
-console.log(heap.extractMax());
-console.log(heap.vals);
+
+let ER = new PriorityQueue();
+console.log(ER.enqueue("cold", 5));
+console.log(ER.enqueue("gunshot", 1));
+console.log(ER.enqueue("high fever", 4));
+console.log(ER.enqueue("broken arm", 2));
+console.log(ER.enqueue("glass in foot", 3));
+console.log(ER.dequeue());
+console.log(ER.dequeue());
+console.log(ER.dequeue());
+console.log(ER.dequeue());
+console.log(ER.dequeue());
